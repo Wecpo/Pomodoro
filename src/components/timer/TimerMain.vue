39c802a64f2a862d028b95ebc392706d282974f2
Senin Maksim <wecpo@yandex.ru>
2024-11-13 15:18:42 +0300

    refactor: created useTimer composable.

--- src/components/timer/TimerMain.vue
+++ src/components/timer/TimerMain.vue
@@ -1,20 +1,15 @@
 <script setup lang="ts">
 import type { TimerSettings } from '@/types/interfaces/TimerSettings';
-import type { TimerState } from '@/types/interfaces/TimerState';
 import IconForwardButton from '@/components/icons/IconForwardButton.vue';
 import IconSettings from '@/components/icons/IconSettings.vue';
 import TimerButton from '@/components/timer/TimerButton.vue';
 import TimerProgressBar from '@/components/timer/TimerProgressBar.vue';
 import TimerSettingsModal from '@/components/timer/TimerSettingsModal.vue';
-import { useFaviconHref } from '@/composable/useFaviconHref';
+import { useTimer } from '@/composable/useTimer';
 import { useTimerTypeKey } from '@/composable/useTimerTypeKey';
-import { useTitle } from '@/composable/useTitle';
 import { TIMER_STATUS, TIMER_TYPE } from '@/types/enums/Timer';
 import { formatTime } from '@/utils/formatTime';
-import { computed, onMounted, onUnmounted, reactive, ref, toRef, watchEffect } from 'vue';
-import { useToast } from 'vue-toastification';
-
-const toast = useToast();
+import { computed, onMounted, onUnmounted, reactive, ref, toRef } from 'vue';
 
 const DEFAULT_TIMER_SETTINGS = {
   focusDuration: 1800,
@@ -24,18 +19,12 @@ const DEFAULT_TIMER_SETTINGS = {
   timerFormat: 'minutes',
 };
 
-const timerSettings = reactive<TimerSettings>(DEFAULT_TIMER_SETTINGS);
-const timerState = reactive<TimerState>({
-  timerValue: timerSettings.focusDuration,
-  roundCounter: 1,
-  timerType: TIMER_TYPE.FOCUS,
-  timerStatus: TIMER_STATUS.PAUSED,
-});
-
 const showModal = ref(false);
 const settingsIconRef = ref<HTMLElement | null>(null);
+
+const timerSettings = reactive<TimerSettings>(DEFAULT_TIMER_SETTINGS);
+const { timerState, startTimer, pauseTimer, changeTimer, intervalId } = useTimer(timerSettings);
 const { timerTypeKey } = useTimerTypeKey(toRef(timerState, 'timerType'));
-let intervalId = 0;
 
 function fetchSettings() {
   const settingsData = localStorage.getItem('timerSettings');
@@ -63,69 +52,6 @@ function fetchSettings() {
 const isTimerPaused = computed(() => timerState.timerStatus === TIMER_STATUS.PAUSED);
 const isTimerStarted = computed(() => timerState.timerStatus === TIMER_STATUS.STARTED);
 
-function changeTimerToFocus() {
-  if (timerState.timerType === TIMER_TYPE.SHORT_BREAK) {
-    timerState.timerType = TIMER_TYPE.FOCUS;
-    timerState.timerValue = timerSettings.focusDuration;
-    pauseTimer();
-    toast.info('Короткий перерыв завершен!');
-    return;
-  }
-  if (timerState.timerType === TIMER_TYPE.LONG_BREAK) {
-    timerState.roundCounter = 1;
-    timerState.timerType = TIMER_TYPE.FOCUS;
-    timerState.timerValue = timerSettings.focusDuration;
-    pauseTimer();
-    toast.info('Длинный перерыв завершен!');
-  }
-}
-
-function changeTimerToBreak() {
-  if (timerState.roundCounter < timerSettings.rounds) {
-    timerState.roundCounter++;
-    timerState.timerType = TIMER_TYPE.SHORT_BREAK;
-    timerState.timerValue = timerSettings.shortBreakDuration;
-    pauseTimer();
-    toast.success('Помидор завершен!');
-    return;
-  }
-
-  if (timerState.roundCounter === timerSettings.rounds) {
-    timerState.roundCounter++;
-    timerState.timerType = TIMER_TYPE.LONG_BREAK;
-    timerState.timerValue = timerSettings.longBreakDuration;
-    pauseTimer();
-    toast.success('Помидор завершен!');
-  }
-}
-
-function startTimer() {
-  timerState.timerStatus = TIMER_STATUS.STARTED;
-
-  intervalId = setInterval(() => {
-    timerState.timerValue--;
-    if (timerState.timerValue < 0) {
-      clearInterval(intervalId);
-      changeTimer();
-    }
-  }, 1000);
-}
-
-function pauseTimer() {
-  clearInterval(intervalId);
-  timerState.timerStatus = TIMER_STATUS.PAUSED;
-}
-
-const changeTimerMap = new Map<TIMER_TYPE, () => void>([
-  [TIMER_TYPE.FOCUS, changeTimerToBreak],
-  [TIMER_TYPE.LONG_BREAK, changeTimerToFocus],
-  [TIMER_TYPE.SHORT_BREAK, changeTimerToFocus],
-]);
-
-function changeTimer() {
-  changeTimerMap.get(timerState.timerType)?.();
-}
-
 const timerClass = computed(() => {
   if (timerState.timerType === TIMER_TYPE.FOCUS) {
     return 'timer--backgound--focus';
@@ -136,20 +62,6 @@ const timerClass = computed(() => {
   }
 
   return 'timer--background--long-break';
-});
-
-watchEffect(() => {
-  const { title } = useTitle(timerState.timerValue, timerState.timerType);
-  document.title = title;
-});
-
-watchEffect(() => {
-  const faviconLink: HTMLLinkElement
-  = document.querySelector('link[rel*=\'shortcut icon\']') || document.createElement('link');
-
-  const { faviconLinkHref } = useFaviconHref(timerState.timerStatus, timerState.timerType);
-
-  faviconLink.href = faviconLinkHref;
 });
 
 onMounted(() => {
